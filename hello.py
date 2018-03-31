f00f495c76945b270145b643aac73ae792bedb56
pjkonicki <pjkonicki@gmail.com>
2018-03-31 00:05:56 -0500

    Added sentence

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ Created on Thu Apr 20 22:50:57 2017
 
 def hello():
     """Print "Hello World" and return None"""
-    print("Hello World")
+    print("Hello World. It's nice to meet you.")
 
 # main program starts here
 hello()
